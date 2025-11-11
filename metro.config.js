17d867cd159c2a49c28064fc47f8e73508c84fef
youssefmohamed67896-cyber <youssefmohamed67896@gmail.com>
2025-11-11 15:36:43 +0000

    Update code

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// ✅ دي الإضافة المهمة عشان يعرف يتعامل مع الخطوط
-config.resolver.assetExts.push('ttf');
 
 module.exports = config;
